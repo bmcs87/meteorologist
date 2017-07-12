03e900832be63108ecd3f2f11cc5932b4ff7ab8a
Becca Smith <rsmith71@chicagobooth.edu>
2017-07-12 20:51:28 +0000

    final

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -17,19 +17,20 @@ class ForecastController < ApplicationController
     # ==========================================================================
 
   
-    url = "https://api.darksky.net/forecast/7061017e8de3ec9d3d32af1b8ef4067f/"
-    url = url + @lat, +@lng
-    parsed_data = JSON.parse(open(url).read)
+    @url = "https://api.darksky.net/forecast/7061017e8de3ec9d3d32af1b8ef4067f/"
+    
+    @url = @url + @lat +"," +@lng
+    parsed_data = JSON.parse(open(@url).read)
   
-    #@current_temperature = parsed_data["latitude"]["longitude"]["timezone"]["offset"]["currently"]["temperature"]
+   
     @current_temperature = parsed_data.dig("currently", "temperature")
-    #@current_summary = parsed_data["latitude"]["longitude"]["timezone"]["offset"]["currently"]["summary"]
+    
     @current_summary = parsed_data.dig("currently", "summary")
-    #@summary_of_next_sixty_minutes = parsed_data["latitude"]["longitude"]["timezone"]["offset"]["minutely"]
+ 
     @summary_of_next_sixty_minutes = parsed_data.dig("minutely","summary")
-    #@summary_of_next_several_hours = parsed_data["latitude"]["longitude"]["timezone"]["offset"]["hourly"]
+    
     @summary_of_next_several_hours = parsed_data.dig("hourly", "summary")
-    #@summary_of_next_several_days = parsed_data["latitude"]["longitude"]["timezone"]["offset"]["daily"]
+    
     @summary_of_next_several_days = parsed_data.dig("weekly","summary")
     render("forecast/coords_to_weather.html.erb")
   end
--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -9,20 +9,16 @@ class GeocodingController < ApplicationController
   def street_to_coords
     @street_address = params[:user_street_address]
     
-    # convert street address from array to string with +'s between each word, add it onto url
-
     # ==========================================================================
     # Your code goes below..
     #
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
 
-    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address="
     
     #@street_address = @street_address.gsub(\s+/,"+")
-    #why doesn't the @street_address work?  it seems like that formula should work
-    #url.join(@street_address)
-    
+  
     parsed_data = JSON.parse(open(url).read)
 
     @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
